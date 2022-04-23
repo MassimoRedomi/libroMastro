df52724d7be334f9da4d9078287b3e990590cfff
Jose Santos L <josesantosl1607@gmail.com>
2022-04-23 18:42:56 +0200

    documentando updateAll.sh

--- updateAll.sh
+++ updateAll.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Questo script serve per aggiornare tutti i file che hanno un markdown modificato.
+#Anche aggiorna il README.md in base a tutti i file.
+#E finalmente compila il programma.
 python3 babel/babel.py tangle Structs.md
 python3 babel/babel.py tangle User.md
 python3 babel/babel.py tangle Node.md
