e97b1df82e41dc1693c708518b10ed25010b55ee
Jose Santos L <josesantosl1607@gmail.com>
2022-04-23 22:15:25 +0200

    change tangle to translate

--- updateAll.sh
+++ updateAll.sh
@@ -2,9 +2,9 @@
 #Questo script serve per aggiornare tutti i file che hanno un markdown modificato.
 #Anche aggiorna il README.md in base a tutti i file.
 #E finalmente compila il programma.
-python3 babel/babel.py tangle Structs.md
-python3 babel/babel.py tangle User.md
-python3 babel/babel.py tangle Node.md
-python3 babel/babel.py tangle main.md
+python3 babel/babel.py translate Structs.md
+python3 babel/babel.py translate User.md
+python3 babel/babel.py translate Node.md
+python3 babel/babel.py translate main.md
 python3 babel/babel.py readme Structs.md Node.md User.md main.md
 make
